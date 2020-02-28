f215bb2b2d033fb34be22c55c334f3c994868bdd
dennischen123 <dennischen123@gmail.com>
2020-02-28 10:09:51 -0800

    User and Photo Model added

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const Restaurant = require('./Restaurant');
 const Review = require('./Review');
+const Photo = require('./Photo');
+
 
 const restaurantSchema = new mongoose.Schema({
 	name: String,
 	foodType: String,
-	photos: [String],
+	photos: [Photo.schema],
 	reviews: [Review.schema]
 });
 
