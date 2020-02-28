2f0283a5ff1cc01de38828389a237cabede53861
danielj-francisco <johnd.francisco1@gmail.com>
2020-02-28 15:13:22 -0800

    saved in submaster on accident

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,12 +1,35 @@
 const db = require('../models');
-// TEMPLATE:
-// const index = (req, res) => {
-//     // We access our datbase through the db variable
-//     db.Restaurant.find({})
-//       .populate('posts.user', 'firstName lastName _id')
-//       .exec((err, foundCities) => {
-//         if (err) return res.json(err);
-    
-//         res.json(foundCities);
-//       });
-// };
+
+const index = (req,res) => {
+    db.Restaurant.find({})
+        .populate('restaurants.name', 'name foodtype')
+        .exec((err, allRestaurants) =>{
+            if (err) return res.json(err);
+            res.json(allRestaurants);
+    });
+}
+
+const show = (req, res) => {
+    db.Restaurant.findById(req.params.id, (err, restaurantPage) => {
+    if (err) return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
+  
+    res.json(restaurantPage);
+    });
+    console.log()
+};
+
+const create = (req, res) => {
+    console.log(req.body); 
+
+    db.Restaurant.create(req.body, (err, addRestaurant) => {
+    if (err) return res.json(err);
+
+    res.json(addRestaurant);
+    });
+};
+
+module.exports = {
+    index: index,
+    show: show,
+    create: create,
+}
